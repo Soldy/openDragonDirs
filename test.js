2fa64c42fc02bcd79e055031378d6f8a4b6e4a5a
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-05 11:57:04 +0000

    test parts
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ test.js
@@ -0,0 +1,82 @@
+const nanoTest  = new (require('nanoTest')).test({
+    'debugPrint' : 'short'
+});
+const openDragonDirs = (require('./index.js')).dirs;
+
+nanoTest.add(
+    'db',
+    {
+        'function':openDragonDirs.db
+    },
+    'j==',
+    [
+       '.db',
+    ]
+);
+nanoTest.add(
+    'cdn',
+    {
+        'function':openDragonDirs.cdn
+    },
+    'j==',
+    [
+       'cdn',
+    ]
+);
+
+nanoTest.add(
+    'www',
+    {
+        'function':openDragonDirs.www
+    },
+    'j==',
+    [
+       'www'
+    ]
+);
+
+nanoTest.add(
+    'extension',
+    {
+        'function':openDragonDirs.extension
+    },
+    'j==',
+    [
+       'extension'
+    ]
+);
+
+nanoTest.add(
+    'components',
+    {
+        'function':openDragonDirs.components
+    },
+    'j==',
+    [
+       'css',
+       'html',
+       'js',
+       'svg'
+    ]
+);
+
+
+nanoTest.add(
+    'all',
+    {
+        'function':openDragonDirs.all
+    },
+    'j==',
+    [
+       '.db',
+       'cdn',
+       'www',
+       'www/css',
+       'www/html',
+       'www/js',
+       'www/svg',
+       'extension'
+    ]
+);
+
+nanoTest.run();
